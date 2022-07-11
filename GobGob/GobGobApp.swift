fd15629d40648e07523ba68be0d2b9d53c20c63b
taisei <ninzin60en.dev@gmail.com>
2022-07-11 22:33:45 +0900

    Initial Commit

---
+++ GobGob/GobGobApp.swift
@@ -0,0 +1,17 @@
+//
+//  GobGobApp.swift
+//  GobGob
+//
+//  Created by onakama on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct GobGobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
